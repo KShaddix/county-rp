358a471423695e05c6d1a4cd81d8eab493fe4a4c
bitbodrit <ya.aibulat1@gmail.com>
2021-05-14 19:03:56 +0300

    Added ForumController

---
+++ src/Services/Forum/Models/Api/ApiForumDtoIn.cs
@@ -0,0 +1,22 @@
+﻿namespace CountyRP.Services.Forum.Models.Api
+{
+    public class ApiForumDtoIn
+    {
+        public string Name { get; }
+
+        public int ParentId { get; }
+
+        public int Order { get; }
+
+        public ApiForumDtoIn(
+            string name,
+            int parentId,
+            int order
+        )
+        {
+            Name = name;
+            ParentId = parentId;
+            Order = order;
+        }
+    }
+}
---
+++ src/Services/Forum/Models/Api/ApiForumDtoOut.cs
@@ -0,0 +1,26 @@
+﻿namespace CountyRP.Services.Forum.Models.Api
+{
+    public class ApiForumDtoOut
+    {
+        public int Id { get; }
+
+        public string Name { get; }
+
+        public int ParentId { get; }
+
+        public int Order { get; }
+
+        public ApiForumDtoOut(
+            int id,
+            string name,
+            int parentId,
+            int order
+        )
+        {
+            Id = id;
+            Name = name;
+            ParentId = parentId;
+            Order = order;
+        }
+    }
+}
